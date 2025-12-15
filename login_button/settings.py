3dee151ffc2b1622b7a778eb63f19f97e9cdbf33
unknown <shahsafalta0@gmai.com>
2025-12-15 21:40:30 +0545

    setting change

--- login_button/settings.py
+++ login_button/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f(q-rv^$s#v#2f&@v9wlf+sm$n=pfkip5r+(6-9@)f#dnflu#3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
